e92581ea960b378380f3c2df36276e65afc9f271
unknown <boedi15@gmail.com>
2021-12-12 22:32:29 -0700

    added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,13 +3,10 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "../models/transactions.js",
-    // "./css/style.css",
-    // "./js/idb.js",
-    // "./js/index.js",
-    // "./index.html",
-    // "../routes/api.js",
-    // "../server.js"
+    "./js/idb.js",
+    "./js/index.js",
+    "./index.html",
+    "./css/styles.css"
 
 ]
 
@@ -18,6 +15,42 @@ self.addEventListener('install', function (e) {
       caches.open(CACHE_NAME).then(function (cache) {
         console.log('installing cache : ' + CACHE_NAME)
         return cache.addAll(FILES_TO_CACHE)
+      }).catch(err => console.log(err))
+    )
+  })
+
+  self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+
       })
     )
   })
